6008b97edc1be545b1519c5039213488b925a41d
JakubRostowski <rostowskijakub@gmail.com>
2022-06-09 19:26:50 +0200

    zadanie 3 i 4

--- src/model/Human.java
+++ src/model/Human.java
@@ -1,7 +1,13 @@
 package model;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Human {
     private String name;
+
+    private Double salary;
     private Animal pet;
 
     private Car car;
@@ -16,5 +22,23 @@ public class Human {
 
     public void getACar(Car car) {
         this.car = car;
+    }
+
+    public Double getSalary() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar. getInstance());
+        System.out.println(timeStamp + " - " + this.salary);
+        return this.salary;
+    }
+
+    public void setSalary(Double salary) {
+        if (salary >= 0) {
+            this.salary = salary;
+            System.out.println("Data has been sent to accounting system.");
+            System.out.println("Remember to get annex to the contract from Mrs. Hania from HR.");
+            System.out.println("ZUS and US already know about your income.");
+        } else {
+            System.out.println("Salary can't be negative!");
+        }
+
     }
 }
